3f27a1651f6dfd861a8e820fc5a064f913112fa5
JML Alcaraz <68015933+juanlopezcode@users.noreply.github.com>
2024-09-24 12:08:31 +0200

    Add files via upload

---
+++ src/clinical_ts/ts/s4_modules/s4_utils.py
@@ -0,0 +1,28 @@
+# adapted from https://github.com/state-spaces/s4/blob/main/models/s4/s4.py
+import torch
+import torch.nn as nn
+from einops import rearrange
+
+class DropoutNd(nn.Module):
+    def __init__(self, p: float = 0.5, tie=True, transposed=True):
+        """
+        tie: tie dropout mask across sequence lengths (Dropout1d/2d/3d)
+        """
+        super().__init__()
+        if p < 0 or p >= 1:
+            raise ValueError("dropout probability has to be in [0, 1), " "but got {}".format(p))
+        self.p = p
+        self.tie = tie
+        self.transposed = transposed
+        self.binomial = torch.distributions.binomial.Binomial(probs=1-self.p)
+
+    def forward(self, X):
+        """X: (batch, dim, lengths...)."""
+        if self.training:
+            if not self.transposed: X = rearrange(X, 'b ... d -> b d ...')
+            mask_shape = X.shape[:2] + (1,)*(X.ndim-2) if self.tie else X.shape
+            mask = torch.rand(*mask_shape, device=X.device) < 1.-self.p
+            X = X * mask * (1.0/(1-self.p))
+            if not self.transposed: X = rearrange(X, 'b d ... -> b ... d')
+            return X
+        return X
